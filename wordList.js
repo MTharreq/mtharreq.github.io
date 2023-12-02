817b2a6a7e34ceb8d7ce4a14529cb649832e8692
MTharreq <mtharreq@gmail.com>
2023-12-03 03:29:23 +0700

    update: word list

--- wordList.js
+++ wordList.js
@@ -1,71 +1,70 @@
 const listTitle = [
-    // A
-    'AWOKW AWOKW',
-    // B
-    'Baik Baik',
-    // C
-    // D
-    // E
-    // F
-    // G
-    'Gweh Banget',
-    'Gweh Gantenk',
-    'Gweh Keren',
-    // H
-    "Ha'aa Laah",
-    'Halo Cantik',
-    'Halo Ganteng',
-    'Hello Girls.',
-    'Hello World',
-    'Hihi Haha',
-    'Huhu Heho',
-    'Holla Weirdo.',
-    // I
-    // J
-    'Jirr Laah!',
-    // K
-    'Keren Banget',
-    'Knp Bro?',
-    'Knp Lah?',
-    // L
-    'Looo Keren',
-    'Lo Jelek',
-    'Lo Keren',
-    'Lucu Gak?',
-    "Luuh Keren",
-    // M
-    'Maaf Laah',
-    'Masa Sih?',
-    // N
-    "Not Defined",
-    // O
-    "Oke Sip",
-    // P
-    // Q
-    // R
-    'Reqq Reqq',
-    'Resaa Jelek',
-    'Resaa Cantik',
-    'Resaa Aneh!!',
-    // S
-    'Siap Pak!!',
-    // T
-    'Tareq Krenn',
-    'Tareq Gantenk',
-    // U
-    // V
-    // W
-    'Wasup Bro',
-    'Wasup Bro',
-    'Weee Wooo',
-    'Wihii Wuhuu',
-    'Wkwk Wkwk',
-    // X
-    // Y
-    // Z
-    'zzz zzz',
-    '... Hah?',
-    '... Huh?',
-    '... Heh?',
-    '10110 10110'
-]
+  // A
+  // "AWOKW AWOKW",
+  // B
+  // 'Baik Baik',
+  // C
+  // D
+  // E
+  // F
+  // G
+  // 'Gweh Banget',
+  // 'Gweh Gantenk',
+  // 'Gweh Keren',
+  // H
+  // "Ha'aa Laah",
+  // 'Halo Cantik',
+  // 'Halo Ganteng',
+  // 'Hello Girls.',
+  "Hello World",
+  // "Hihi Haha",
+  // "Huhu Heho",
+  // "Holla Weirdo.",
+  // I
+  // J
+  // 'Jirr Laah!',
+  // K
+  // 'Keren Banget',
+  // 'Knp Bro?',
+  // 'Knp Lah?',
+  // L
+  // 'Looo Keren',
+  // 'Lo Jelek',
+  // 'Lo Keren',
+  // 'Lucu Gak?',
+  // "Luuh Keren",
+  // M
+  // 'Maaf Laah',
+  // 'Masa Sih?',
+  // N
+  // "Not Defined",
+  // O
+  // "Oke Sip",
+  // P
+  // Q
+  // R
+  // "Reqq Reqq",
+  // "Resaa Jelek",
+  // 'Resaa Cantik',
+  // 'Resaa Aneh!!',
+  // S
+  // 'Siap Pak!!',
+  // T
+  // 'Tareq Krenn',
+  // "Tareq Gantenk",
+  // U
+  // V
+  // W
+  // 'Wasup Bro',
+  // "Weee Wooo",
+  // "Wihii Wuhuu",
+  // "Wkwk Wkwk",
+  // X
+  // Y
+  // Z
+  // "zzz zzz",
+  // "... Hah?",
+  // "... Huh?",
+  // "... Heh?",
+  "10110 10110",
+];
